322c581902c25508e407429cfbc7eaf711f22979
MH <qprs71@gmail.com>
2025-11-26 15:00:17 +0800

    use 'xvfb-run' to launch headed browser

--- src/availability-checker.ts
+++ src/availability-checker.ts
@@ -31,7 +31,7 @@ class AvailabilityChecker {
 
   async check(): Promise<void> {
     const browser = await chromium.launch({
-      headless: true  // Set to true for background running
+      headless: false  // Set to true for background running
     });
 
     const context = await browser.newContext({
