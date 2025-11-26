bf33c3aadc9999a71c0f04334eb384602f2369a2
MH <qprs71@gmail.com>
2025-11-26 11:18:24 +0800

    update 'module' to 'NodeNext' in tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import AvailabilityChecker from './availability-checker';
+import AvailabilityChecker from './availability-checker.js';
 
 const URL = 'https://inline.app/booking/-NeqTSgDQOAYi30lg4a7:inline-live-3/-NeqTStJZDIBQHEMSDI8'; // Taipei
 // const URL = 'https://inline.app/booking/-NeqTSgDQOAYi30lg4a7:inline-live-3/-OUYVD5L8af9l-fOxBi5'; // kaohsiung
